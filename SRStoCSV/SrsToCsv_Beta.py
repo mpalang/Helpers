a7c9556e14f4c3ed3601467754cc6c37ee204cbc
Moritz Lang <moritz-lang(at)gmx.net>
2021-12-13 10:04:49 +0100

    1

--- SRStoCSV/SrsToCsv_Beta.py
+++ SRStoCSV/SrsToCsv_Beta.py
@@ -3,7 +3,9 @@ from srsLib import srsFile
 #Make your settings from here: -->
 # srs File:    
 # Can be String or List of Strings. Leave empty if all .srs files shall be converted from folder.
-filename= 'SRStoCSV/Test.srs' #output_filename='_Converted' # will be attached to the input filename and has to be added to the getSRSData Function.
+filename= 'Test.srs' #output_filename='_Converted' # will be attached to the input filename and has to be added to the getSRSData Function.
+
+#TODO: Make settings adjustable...for now just default values in srsLib work!
 #Average Number for one spectrum (from experiment settings).
 No_Avg= 200
 # add as many times for Differencespectra as you want. 
@@ -15,5 +17,6 @@ M_No= 300
 
 X=srsFile(filename)
 X.getData()
+Data=X.Data
 
-print(f'{X.Spec_Pts}\n{X.info}')
+#print(f'{X.Spec_Pts}\n{X.info}')
--- SRStoCSV/srsLib.py
+++ SRStoCSV/srsLib.py
@@ -1,8 +1,7 @@
-#import os,glob
-#from pathlib import Path
-import numpy as np
+#import os
+#import glob
 from pathlib import Path
-#import matplotlib.pyplot as plt
+import numpy as np
 import logging
 import sys
 logging.basicConfig(
@@ -13,14 +12,21 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ])
 
+# def getFileList(Settings):
+#     Settings['parent_directory']=Path(__file__).parent    
+#     if Settings['filename'] == '':
+#         Settings['filelist']=glob.glob(str(Path(Settings['parent_directory'],'*.srs'))) 
+#     else: 
+#         Settings['filelist']=Settings['filename']
+#     return Settings
+
 class srsFile:
     def __init__(self,filename):
         self.Filename=Path(filename)
         self.Data={}
         self.Spec_Pts = np.fromfile(self.Filename, dtype=np.int32 , count=1, offset=14036)[0]
-        Max_Wn = np.fromfile(self.Filename, dtype=np.single , count=1, offset=14048)[0]
-        Min_Wn = np.fromfile(self.Filename, dtype=np.single , count=1, offset=14052)[0]
-        self.Wn=np.linspace(Min_Wn, Max_Wn, num=self.Spec_Pts)
+        self.Max_Wn = np.fromfile(self.Filename, dtype=np.single , count=1, offset=14048)[0]
+        self.Min_Wn = np.fromfile(self.Filename, dtype=np.single , count=1, offset=14052)[0]
         info_raw=np.fromfile(self.Filename, dtype='S1',count=318, offset=15232)
         self.info=''
         for b in info_raw:
@@ -28,7 +34,40 @@ class srsFile:
         del b 
    
 
-    def getData(self):
+    def getData(self,No_Avg=200,Header_Size=25,Footer_Size=25,Start_of_Data=49232,Percent=False,M_No=500):
+#        Start_of_Data=49232 #4226*4 #original Value from Matlab script: 16904
+#        Default Values must be adjusted to current experiment!
+        
         f=Path(self.Filename)
-        logging.info(f'Get Data from {f}.')
+        logging.info(f'Get Data from {f}.')       
+        
+        Wn=np.linspace(self.Min_Wn, self.Max_Wn, num=self.Spec_Pts)
+        spectra=[]
+        time=[]
+
+        bg=np.fromfile(f, dtype=np.single , count=self.Spec_Pts, offset= 16904 + 4*(Header_Size))
+                
+        i=0
+        
+        s=np.fromfile(f, dtype=np.single , count=self.Spec_Pts, offset= Start_of_Data+4*(Header_Size))
+        scr=np.fromfile(f, dtype=np.uint, count=2, offset= Start_of_Data)
+        s_count=1
+        
+        while scr.shape[0]>1 and scr[0]==No_Avg and i<M_No:
+            try:
+                spectra.append(s)
+                time.append(scr[1]/6000)
+                i=i+1
+                s_count=i
+                s=np.fromfile(f, dtype=np.single , count=self.Spec_Pts, offset= Start_of_Data+4*Header_Size+4*i*(self.Spec_Pts+Header_Size))
+                scr=np.fromfile(f, dtype=np.uint, count=2, offset= Start_of_Data + 4*i*(Header_Size+self.Spec_Pts))
+            except:
+                spectra.append('Error')
+                time.append('Error')
+                
+        #spectra=spectra/100
+        
+        self.Data={'Wn': Wn, 'time': time, 'spectra': spectra, 'number of spectra': s_count, 'Bg': bg}
+        
+        return self
 
